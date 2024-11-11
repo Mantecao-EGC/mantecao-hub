2dbdd7a1952eaa2a0710e7cee0fe4a5414c8abb6
Samuel Albalat <saoalbalat@gmail.com>
2024-11-11 13:57:50 -0500

    fix(test): cambios a common de Selenium
    Se ha añadido un nuevo driver para Firefox

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as SC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as SF
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 def initialize_driver():
@@ -8,8 +10,22 @@ def initialize_driver():
     options = webdriver.ChromeOptions()
 
     # Initialise the browser using WebDriver Manager
-    service = Service(ChromeDriverManager().install())
+    service = SC(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+
+def initialize_driver_firefox():
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # Quitar "--headless" si quieres ver el navegador en modo visible
+    # options.add_argument("--headless")  # Puedes quitar esto para modo visible
+
+    # Iniciar el driver de Firefox usando webdriver-manager
+    service = SF(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
     return driver
 
 
