44f51408f2190b38ebea684e3399978af0b1a1b8
nerjimado <nerjimado@alum.us.es>
2024-11-11 12:57:26 +0100

    feat(Test): Test unitarios añadidos y editados
    Se han añadido 3 nuevos tests unitarios y se han editado el resto añadiendo el campo is_developer

--- app/modules/auth/tests/test_unit.py
+++ app/modules/auth/tests/test_unit.py
@@ -12,10 +12,10 @@ def test_client(test_client):
     Extends the test_client fixture to add additional specific data for module testing.
     """
     with test_client.application.app_context():
+
         # Add HERE new elements to the database that you want to exist in the test context.
         # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
         pass
-
     yield test_client
 
 
@@ -51,16 +51,27 @@ def test_login_unsuccessful_bad_password(test_client):
 
 def test_signup_user_no_name(test_client):
     response = test_client.post(
-        "/signup", data=dict(surname="Foo", email="test@example.com", password="test1234"), follow_redirects=True
+        "/signup", data=dict(surname="Foo", email="test@example.com", password="test1234",
+                             is_developer=False), follow_redirects=True
     )
     assert response.request.path == url_for("auth.show_signup_form"), "Signup was unsuccessful"
     assert b"This field is required" in response.data, response.data
 
 
+def test_signup_user_no_developer(test_client):
+    response = test_client.post(
+        "/signup", data=dict(surname="Foo", name="Faa", email="test@example.com", password="test1234"
+                             ), follow_redirects=True
+    )
+    assert response.request.path == url_for("auth.show_signup_form"), "Signup was unsuccessful"
+    assert pytest.raises(ValueError, match="Is_developer is required.")
+
+
 def test_signup_user_unsuccessful(test_client):
     email = "test@example.com"
     response = test_client.post(
-        "/signup", data=dict(name="Test", surname="Foo", email=email, password="test1234"), follow_redirects=True
+        "/signup", data=dict(name="Test", surname="Foo", email=email, password="test1234",
+                             is_developer=False), follow_redirects=True
     )
     assert response.request.path == url_for("auth.show_signup_form"), "Signup was unsuccessful"
     assert f"Email {email} in use".encode("utf-8") in response.data
@@ -69,17 +80,34 @@ def test_signup_user_unsuccessful(test_client):
 def test_signup_user_successful(test_client):
     response = test_client.post(
         "/signup",
-        data=dict(name="Foo", surname="Example", email="foo@example.com", password="foo1234"),
+        data=dict(name="Foo", surname="Example", email="foo@example.com", password="foo1234",
+                  is_developer=False),
         follow_redirects=True,
     )
     assert response.request.path == url_for("public.index"), "Signup was unsuccessful"
 
 
-def test_service_create_with_profie_success(clean_database):
+def test_signup_developer_user_successful(test_client):
+    response = test_client.post(
+        "/signup",
+        data={
+            "name": "Test",
+            "surname": "Foo",
+            "email": "service_test@example.com",
+            "is_developer": False,
+            "password": "test1234"
+        },
+        follow_redirects=True,
+    )
+    assert response.request.path == url_for("public.index"), "Signup was successful"
+
+
+def test_service_create_with_profile_success(clean_database):
     data = {
         "name": "Test",
         "surname": "Foo",
         "email": "service_test@example.com",
+        "is_developer": True,
         "password": "test1234"
     }
 
@@ -113,6 +141,22 @@ def test_service_create_with_profile_fail_no_password(clean_database):
     }
 
     with pytest.raises(ValueError, match="Password is required."):
+        AuthenticationService().create_with_profile(**data)
+
+    assert UserRepository().count() == 0
+    assert UserProfileRepository().count() == 0
+
+
+def test_service_create_with_profile_fail_no_developer(clean_database):
+    data = {
+        "name": "Test",
+        "surname": "Foo",
+        "email": "test@example.com",
+        "password": "1234",
+        "is_developer": None
+    }
+
+    with pytest.raises(ValueError, match="Is_developer is required."):
         AuthenticationService().create_with_profile(**data)
 
     assert UserRepository().count() == 0
