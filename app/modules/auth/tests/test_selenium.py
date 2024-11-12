c84a4d75c9276a29912816618c8351c9b0454cf2
alvhidrod <alvhidrod@alum.us.es>
2024-11-12 21:42:14 +0100

    feat: Testeo Selenium de remember me

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -7,13 +7,32 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
 from core.environment.host import get_host_for_selenium_testing
-from core.selenium.common import close_driver, initialize_driver_firefox
+from core.selenium.common import initialize_driver, close_driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+# Configurar Selenium para usar Firefox
+options = webdriver.FirefoxOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# Quitar "--headless" si quieres ver el navegador en modo visible
+#options.add_argument("--headless")  # Puedes quitar esto para modo visible
+
+# Iniciar el driver de Firefox usando webdriver-manager
+service = Service(GeckoDriverManager().install())
 
 
 def test_login_and_check_element():
 
-    driver = initialize_driver_firefox()
-
+    #driver = initialize_driver()
+    driver = webdriver.Firefox(service=service, options=options)
     try:
         host = get_host_for_selenium_testing()
 
@@ -51,7 +70,8 @@ def test_login_and_check_element():
 
 
 def test_login_and_remember_me_true():
-    driver = initialize_driver_firefox()
+
+    driver = webdriver.Firefox(service=service, options=options)
 
     try:
         host = get_host_for_selenium_testing()
@@ -67,8 +87,8 @@ def test_login_and_remember_me_true():
         password_field = driver.find_element(By.NAME, 'password')
         password_field = driver.find_element(By.NAME, 'password')
         driver.find_element(By.NAME, "remember_me").click()
-        email_field.send_keys('user1@example.com')
-        password_field.send_keys('1234')
+        email_field.send_keys('prueba@gmail.com')
+        password_field.send_keys('prueba')
 
         # Send the form
         password_field.send_keys(Keys.RETURN)
@@ -95,7 +115,7 @@ def test_login_and_remember_me_true():
 
 def test_login_and_remember_me_false():
 
-    driver = initialize_driver_firefox()
+    driver = webdriver.Firefox(service=service, options=options)
 
     try:
         host = get_host_for_selenium_testing()
@@ -109,8 +129,9 @@ def test_login_and_remember_me_false():
         # Find the username and password field and enter the values
         email_field = driver.find_element(By.NAME, 'email')
         password_field = driver.find_element(By.NAME, 'password')
-        email_field.send_keys('user1@example.com')
-        password_field.send_keys('1234')
+        password_field = driver.find_element(By.NAME, 'password')
+        email_field.send_keys('prueba@gmail.com')
+        password_field.send_keys('prueba')
 
         # Send the form
         password_field.send_keys(Keys.RETURN)
@@ -133,57 +154,13 @@ def test_login_and_remember_me_false():
 
         # Close the browser
         close_driver(driver)
-
-
-def test_signup_developer_and_check_element():
-
-    driver = initialize_driver_firefox()
-
-    try:
-        host = get_host_for_selenium_testing()
-
-        # Open the login page
-        driver.get(f'{host}/signup')
-
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
-
-        # Find the username and password field and enter the values
-        name_field = driver.find_element(By.NAME, 'name')
-        surname_field = driver.find_element(By.NAME, 'surname')
-        email_field = driver.find_element(By.NAME, 'email')
-        password_field = driver.find_element(By.NAME, 'password')
-        is_developer_field = driver.find_element(By.NAME, 'is_developer')
-
-        name_field.send_keys('Footer')
-        surname_field.send_keys('Looter')
-        email_field.send_keys('user3@example.com')
-        password_field.send_keys('1234')
-        is_developer_field.click()
-
-        # Send the form
-        password_field.send_keys(Keys.RETURN)
-
-        # Wait a little while to ensure that the action has been completed
-        time.sleep(4)
-
-        try:
-
-            driver.find_element(By.XPATH,
-                                "//div[contains(@class, 'col-xl-4 col-lg-12 col-md-12 col-sm-12')"
-                                + " and contains(., 'You are in Developer Mode')]")
-            print('Test passed!')
-
-        except NoSuchElementException:
-            raise AssertionError('Test failed!')
-
-    finally:
-        # Close the browser
-        close_driver(driver)
-
-
 # Call the test function
 
 
+
+
 test_login_and_check_element()
-test_signup_developer_and_check_element()
+
+test_login_and_remember_me_true()
+
+test_login_and_remember_me_false()
