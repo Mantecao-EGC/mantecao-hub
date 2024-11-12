c81b8eafed7b5ba6670a49291c7bfb46ebec8766
alvhidrod <alvhidrod@alum.us.es>
2024-11-12 21:42:14 +0100

    feat: Testeo Selenium de remember me

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -5,12 +5,31 @@ import time
 
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver, close_driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+# Configurar Selenium para usar Firefox
+options = webdriver.FirefoxOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# Quitar "--headless" si quieres ver el navegador en modo visible
+#options.add_argument("--headless")  # Puedes quitar esto para modo visible
+
+# Iniciar el driver de Firefox usando webdriver-manager
+service = Service(GeckoDriverManager().install())
 
 
 def test_login_and_check_element():
 
-    driver = initialize_driver()
-
+    #driver = initialize_driver()
+    driver = webdriver.Firefox(service=service, options=options)
     try:
         host = get_host_for_selenium_testing()
 
@@ -47,5 +66,96 @@ def test_login_and_check_element():
         close_driver(driver)
 
 
+def test_login_and_remember_me_true():
+
+    driver = webdriver.Firefox(service=service, options=options)
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/login')
+
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, 'email')
+        password_field = driver.find_element(By.NAME, 'password')
+        password_field = driver.find_element(By.NAME, 'password')
+        driver.find_element(By.NAME, "remember_me").click()
+        email_field.send_keys('prueba@gmail.com')
+        password_field.send_keys('prueba')
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        driver.delete_cookie('session')
+
+        driver.get(f'{host}/profile/summary')
+        try:
+
+            driver.find_element(By.XPATH, "//h1[contains(@class, 'h3 mb-3') and contains(., 'User profile')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
+
+
+def test_login_and_remember_me_false():
+
+    driver = webdriver.Firefox(service=service, options=options)
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/login')
+
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, 'email')
+        password_field = driver.find_element(By.NAME, 'password')
+        password_field = driver.find_element(By.NAME, 'password')
+        email_field.send_keys('prueba@gmail.com')
+        password_field.send_keys('prueba')
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        driver.delete_cookie('session')
+
+        driver.get(f'{host}/profile/summary')
+        try:
+
+            driver.find_element(By.XPATH, "//h1[contains(@class, 'h2 mb-3') and contains(., 'Login')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
 # Call the test function
+
+
 test_login_and_check_element()
+
+test_login_and_remember_me_true()
+
+test_login_and_remember_me_false()
