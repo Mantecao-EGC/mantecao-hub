c4309a823133a1eefc45e9af7c385ca299d36930
alvhidrod <alvhidrod@alum.us.es>
2024-11-27 20:03:26 +0100

    feat: Arreglo del código del driver de Firefox

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as SF
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 def initialize_driver():
@@ -10,6 +12,20 @@ def initialize_driver():
     # Initialise the browser using WebDriver Manager
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+
+def initialize_driver_firefox():
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # Quitar "--headless" si quieres ver el navegador en modo visible
+    # options.add_argument("--headless")  # Puedes quitar esto para modo visible
+
+    # Iniciar el driver de Firefox usando webdriver-manager
+    service = SF(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
     return driver
 
 
