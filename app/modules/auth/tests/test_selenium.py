ce2bfc112a92464f097bba6e8c6ca4b37e2569a0
Samalbalat <80246257+Samalbalat@users.noreply.github.com>
2024-12-01 21:25:03 +0100

    fix: actualizacion de Selenium Test
    Este commit está hecho para subsanar cambios de cara a la pull request de test de selenium

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -50,6 +50,91 @@ def test_login_and_check_element():
         close_driver(driver)
 
 
+def test_login_and_remember_me_true():
+    driver = initialize_driver_firefox()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/login')
+
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, 'email')
+        password_field = driver.find_element(By.NAME, 'password')
+        password_field = driver.find_element(By.NAME, 'password')
+        driver.find_element(By.NAME, "remember_me").click()
+        email_field.send_keys('user1@example.com')
+        password_field.send_keys('1234')
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        driver.delete_cookie('session')
+
+        driver.get(f'{host}/profile/summary')
+        try:
+
+            driver.find_element(By.XPATH, "//h1[contains(@class, 'h3 mb-3') and contains(., 'User profile')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
+
+
+def test_login_and_remember_me_false():
+
+    driver = initialize_driver_firefox()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/login')
+
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, 'email')
+        password_field = driver.find_element(By.NAME, 'password')
+        email_field.send_keys('user1@example.com')
+        password_field.send_keys('1234')
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        driver.delete_cookie('session')
+
+        driver.get(f'{host}/profile/summary')
+        try:
+
+            driver.find_element(By.XPATH, "//h1[contains(@class, 'h2 mb-3') and contains(., 'Login')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
+
+
 def test_signup_developer_and_check_element():
 
     driver = initialize_driver_firefox()
