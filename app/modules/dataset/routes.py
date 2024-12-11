92ef561111c775e0afb3cd8e6bb23b165976b63f
alvhidrod <alvhidrod@alum.us.es>
2024-12-11 20:03:51 +0100

    feat añadida la paginación

--- app/modules/dataset/routes.py
+++ app/modules/dataset/routes.py
@@ -6,6 +6,8 @@ import tempfile
 import uuid
 from datetime import datetime, timezone
 from zipfile import ZipFile
+from flask_paginate import Pagination
+
 
 from flask import (
     redirect,
@@ -259,9 +261,23 @@ def subdomain_index(doi):
     # Get dataset
     dataset = ds_meta_data.data_set
 
+    count = dataset.get_files_count()
+    files = dataset.feature_models
+    page_num = request.args.get('page', 1, type=int)
+    per_page = 2
+    start_index = ((page_num-1) * per_page)
+    end_index = min(start_index+per_page, count)
+    if end_index > count:
+        end_index = count
+    pagination = Pagination(page=page_num, total=count, per_page=per_page,
+                            display_msg=f'Mostrando archivos {start_index} - {end_index} de un total de  {count}')
+    ls = []
+    for i in range(start_index, end_index):
+        ls.append(files[i])
     # Save the cookie to the user's browser
     user_cookie = ds_view_record_service.create_cookie(dataset=dataset)
-    resp = make_response(render_template("dataset/view_dataset.html", dataset=dataset))
+    resp = make_response(render_template("dataset/view_dataset.html", dataset=dataset, pagination=pagination,
+                                         files=ls))
     resp.set_cookie("view_cookie", user_cookie)
 
     return resp
@@ -276,5 +292,18 @@ def get_unsynchronized_dataset(dataset_id):
 
     if not dataset:
         abort(404)
-
-    return render_template("dataset/view_dataset.html", dataset=dataset)
+    count = dataset.get_files_count()
+    files = dataset.feature_models
+    page_num = request.args.get('page', 1, type=int)
+    per_page = 2
+    start_index = ((page_num-1) * per_page)
+    end_index = min(start_index+per_page, count)
+    if end_index > count:
+        end_index = count
+    pagination = Pagination(page=page_num, total=count, per_page=per_page,
+                            display_msg=f'Mostrando archivos {start_index} - {end_index} de un total de  {count}')
+    ls = []
+    for i in range(start_index, end_index):
+        ls.append(files[i])
+    return render_template("dataset/view_dataset.html", dataset=dataset, pagination=pagination,
+                                        files=ls)
