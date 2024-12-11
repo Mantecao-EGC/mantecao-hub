1966d0edef33aa967bbeeb15411bd94a5a7e05ae
Samuel Albalat Ortiz <saoalbalat@gmail.com>
2024-12-11 12:19:58 +0100

    feat: Añadido filtro de blancos en el services.py
    Se ha añadido la capacidad de filtrar los espacios en blanco en los campos de features y productos

--- app/modules/explore/services.py
+++ app/modules/explore/services.py
@@ -6,5 +6,12 @@ class ExploreService(BaseService):
     def __init__(self):
         super().__init__(ExploreRepository())
 
-    def filter(self, query="", sorting="newest", publication_type="any", tags=[], **kwargs):
-        return self.repository.filter(query, sorting, publication_type, tags, **kwargs)
+    def filter(self, query="", sorting="newest", publication_type="any", tags=[],
+               config_number=0, core_features=0, **kwargs):
+
+        if config_number == '' or int(config_number) < 0:
+            config_number = 0
+        if core_features == '' or int(core_features) < 0:
+            core_features = 0
+
+        return self.repository.filter(query, sorting, publication_type, tags, config_number, core_features, **kwargs)
