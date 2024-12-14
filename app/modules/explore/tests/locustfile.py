62b082fa00954c94cbf294eb0c08dc5ce4fccdda
Samuel Albalat <saoalbalat@gmail.com>
2024-12-14 13:54:22 -0500

    enhancement: Añadidas task de locust para explore
    Se ha añadido dos tasks para el test de carga de explore

--- app/modules/explore/tests/locustfile.py
+++ app/modules/explore/tests/locustfile.py
@@ -1,9 +1,44 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, TaskSet, task, between  # type: ignore
+from core.locust.common import get_csrf_token
+
 
 class ExploreTasks(TaskSet):
     @task
     def explore_page(self):
         self.client.get("/explore")
+
+    @task
+    def search_features_only(self):
+        response = self.client.get("/explore")
+        csrf_token = get_csrf_token(response)
+
+        response = self.client.post("/explore", data={
+            "csrf_token": csrf_token,
+            "query": "",
+            "publication_type": "any",
+            "sorting": "newest",
+            "config_number": "",
+            "core_features": str(between(1, 100))
+        })
+        if response.status_code != 200:
+            print(f"Filter failed: {response.status_code}")
+
+    @task
+    def search_configs_only(self):
+        response = self.client.get("/explore")
+        csrf_token = get_csrf_token(response)
+
+        response = self.client.post("/explore", data={
+            "csrf_token": csrf_token,
+            "query": "",
+            "publication_type": "any",
+            "sorting": "newest",
+            "config_number": str(between(1, 100)),
+            "core_features": ""
+        })
+        if response.status_code != 200:
+            print(f"Filter failed: {response.status_code}")
+
 
 class ExploreUser(HttpUser):
     tasks = [ExploreTasks]
