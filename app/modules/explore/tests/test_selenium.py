91e95cdff6bdd935e1053cc82f7c12a624933170
Nerea Jimenez <nerjimado@alum.us.es>
2024-12-15 17:09:46 +0100

    enhancement: Añadidos dos test de Selenium en explore
    Se han añadido dos tests de Selenium que comprueban el correcto funcionamiento del filtro de los productos(configurations)

--- app/modules/explore/tests/test_selenium.py
+++ app/modules/explore/tests/test_selenium.py
@@ -76,7 +76,79 @@ def test_features_with_empty():
         close_driver(driver)
 
 
+def test_configurations_with_value():
+
+    driver = initialize_driver_firefox()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/explore')
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+        # Find the features field and enter the values
+        config_field = driver.find_element(By.NAME, 'config_number')
+        config_field.clear()
+        config_field.send_keys('2')
+        time.sleep(2)
+        config_field.send_keys('5')
+        # Send the form
+        config_field.send_keys(Keys.RETURN)
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        try:
+
+            driver.find_element(By.XPATH, "//h3[contains(@class, 'h3 mb-3') and contains(., '0 datasets found')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
+
+
+def test_configurations_with_empty():
+
+    driver = initialize_driver_firefox()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f'{host}/explore')
+        # Wait a little while to make sure the page has loaded completely
+        time.sleep(4)
+        # Find the features field and enter the values
+        config_field = driver.find_element(By.NAME, 'config_number')
+        config_field.clear()
+        config_field.send_keys('')
+        # Send the form
+        config_field.send_keys(Keys.RETURN)
+        # Wait a little while to ensure that the action has been completed
+        time.sleep(4)
+
+        try:
+
+            driver.find_element(By.XPATH, "//h3[contains(@class, 'h3 mb-3') and contains(., '4 datasets found')]")
+            print('Test passed!')
+
+        except NoSuchElementException:
+            raise AssertionError('Test failed!')
+
+    finally:
+
+        # Close the browser
+        close_driver(driver)
+
+
 # Call the test functions
 
 test_features_with_value()
 test_features_with_empty()
+test_configurations_with_value()
+test_configurations_with_empty()
